4ee42a14aa00f147a6248322f1361a9394a6c926
Muhammad Hasaan <m.hasaan1999@gmail.com>
2022-01-10 13:05:10 +0500

    Made Header for Home page and changed modal accordingly

--- user/src/App.js
+++ user/src/App.js
@@ -1,8 +1,12 @@
-// import PageLayout from "./components/PageLayout/PageLayout";
+// import PageLayout from "./views/PageLayout/PageLayout";
 import HomePage from "./views/HomePage/HomePage";
 
+// import CustomHeader form './'
 function App() {
-  return <HomePage/>;
+  return( <>
+ <HomePage/>
+  </>
+  )
 }
 
 export default App;
