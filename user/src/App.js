b69827b00cd4953dd4acf8f399a855fd01945da9
Adil Asif <adilasif680@gmai.com>
2022-01-11 21:59:38 +0500

    Add Form

--- user/src/App.js
+++ user/src/App.js
@@ -1,7 +1,7 @@
 
 
 // import PageLayout from "./views/PageLayout/PageLayout";
-import LearningResourcesPage from "./views/LectureVideoPage/LectureVideoPage";
+import LearningResourcesPage from "./views/AddRequestPage/AddRequestPage";
 
 
 function App() {
