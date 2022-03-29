54086a46c071e6d578a3aca80403dfe33475650a
abdurraffaysyed <79164396+abdurraffaysyed@users.noreply.github.com>
2022-03-30 01:09:45 +0500

    first commit for new file innovah.

---
+++ Backend_server/routes/Rewards/reward.js
@@ -0,0 +1,6 @@
+// get req for initial rewards that are unclaimed and display points
+// post req to upload the rewards for the particular user
+// post req when a reward is claimed and  delete that particular
+// reward for that particular user and update the points for that particular user
+
+
