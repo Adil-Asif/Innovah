7beec76f6deada65a9899172648d871120a6454f
abdurraffaysyed <79164396+abdurraffaysyed@users.noreply.github.com>
2022-05-10 01:25:27 +0500

    update learncontroller.

--- Backend_server/controllers/LearningResources/LearnController.js
+++ Backend_server/controllers/LearningResources/LearnController.js
@@ -35,14 +35,14 @@ exports.changeenrollstatus=(req,res)=>{
         var userinfo = session.getItem('useridinfo');
         userinfo="6dbb0ba0-999e-11ec-ba73-d9e1c22c2fb81";
         // var title = session.getItem('playlistname');
-        var title = req.body.playlistname
+        var playid = req.body.playlistid
         // var trainerid = session.getItem('trainerid');
         var trainerid = req.body.trainerid
         console.log(userinfo);
-        title="Algorithms course";
+        // title="Algorithms course";
         trainerid="12"
-        let sql = "update learning set enrolledstatus=1 where userid=? and title=? and trainerid=?";
-        model.query(sql,[userinfo,title,trainerid],(err,result)=>{
+        let sql = "update learning set enrolledstatus=1 where userid=? and playlistid=? and trainerid=?";
+        model.query(sql,[userinfo,playid,trainerid],(err,result)=>{
             if(err){
                 console.log( JSON.stringify(err,undefined,2));
             }
@@ -71,19 +71,28 @@ exports.getallitems=(req,res)=>{
             }
             else{
                 // below 2 lines are useless
-                session.setItem('trainerid',result[0].trainerid);
-                session.setItem('playlistname',result[0].title);
+                // session.setItem('trainerid',result[0].trainerid);
+                // session.setItem('playlistname',result[0].title);
                 // console.log(result)
-                var pic = result[0].imageurl;
-                var title = result[0].title;
-                var desc = result[0].description;
-                var completedstatus = result[0].completedstatus;
-                var enrolledstatus = result[0].enrolledstatus;
-                session.setItem("enrollstatus",enrolledstatus)
+                // var trainerid =
+                Object.keys(result).forEach(function(key) {
+                    // var row = result[key];
+                    var trainerid =result[key].trainerid;
+                    var playlistid = result[key].playlistid; 
+                    var pic = result[key].imageurl;
+                    var title = result[key].title;
+                    var desc = result[key].description;
+                    var completedstatus = result[key].completedstatus;
+                    var enrolledstatus = result[key].enrolledstatus;
+                    res.json({"pic":pic,"title":title,"trainerid":trainerid,"playlistid":playlistid,"desc":desc,"completedstatus":completedstatus,"enrolledstatus":enrolledstatus})
+                    // console.log(row.name)
+                });
                 
-                console.log(result);
+                // session.setItem("enrollstatus",enrolledstatus)
+                
+                // console.log(result);
                 // res.send(pic)
-                res.json({"pic":pic,"title":title,"desc":desc,"completedstatus":completedstatus,"enrolledstatus":enrolledstatus})
+                
             }
         })
     }
@@ -94,25 +103,44 @@ exports.getallitems=(req,res)=>{
 }
 exports.getplaylist=(req,res)=>{
     // trainer is uploading all the resources for the users who requested it.
-    if(session.getItem('signin')==true && session.getItem("enrollstatus")==1){
-        var title = session.getItem('playlistname');
-        var trainerid = session.getItem('trainerid');
+    if(session.getItem('signin')==true ){
+        var title = req.body.playlisttitle;
+        // var playid = req.body.playlistid;
+        // var trainerid = req.body.trainerid;
+        // var title = session.getItem('playlistname');
+        // var trainerid = session.getItem('trainerid');
         var userinfo = session.getItem('useridinfo');
         var playlistname = title;
-        trainerid="12";
+        // trainerid="12";
         userinfo="6dbb0ba0-999e-11ec-ba73-d9e1c22c2fb81";
         title="Algorithms course";
-        let sql = "select * from playlist where trainerid=? AND userid=? AND playlisttitle=?";
-        model.query(sql,[trainerid,userinfo,title],(err,result)=>{
+        let sql = "select * from playlist where userid=? AND playlisttitle=?";
+        model.query(sql,[userinfo,title],(err,result)=>{
             if(err){
                 console.log( JSON.stringify(err,undefined,2));
             }
             else{
                 console.log("query executed properly");
                 if(result.length>0){
-                    session.setItem('videotitle',result[0].videotitle);
-                    session.setItem('videodesc',result[0].description);
-                    res.json({"playlistname":playlistname,"title":result[0].videotitle,"desc":result[0].description,"status":result[0].status});    
+                    Object.keys(result).forEach(function(key) {
+                    // var row = result[key];
+                    var videoid =result[key].id;
+                    var playlisttitle = result[key].playlisttitle;
+                    var trainerid = result[key].trainerid;
+                    var status = result[key].status;
+                    // var desc = result[key].description;
+                     
+                    // var pic = result[key].imageurl;
+                    var title = result[key].videotitle;
+                    var desc = result[key].description;
+                    // var completedstatus = result[key].completedstatus;
+                    // var enrolledstatus = result[key].enrolledstatus;
+                    res.json({"title":title,"trainerid":trainerid,"playlisttitle":playlisttitle,"desc":desc,"status":status,"videoid":videoid})
+                    // console.log(row.name)
+                });
+                    // session.setItem('videotitle',result[0].videotitle);
+                    // session.setItem('videodesc',result[0].description);
+                    // res.json({"playlistname":playlistname,"title":result[0].videotitle,"desc":result[0].description,"status":result[0].status});    
                 }
                 
             }
@@ -122,9 +150,7 @@ exports.getplaylist=(req,res)=>{
         if(session.getItem("signin")==false){
             res.send("please signin first Thankyou");
         }
-        else if(session.getItem("enrollstatus")==0){
-            res.send("please enroll first");
-        }
+        
     }
     
     
@@ -132,7 +158,7 @@ exports.getplaylist=(req,res)=>{
 }
 
 exports.getvideo=(req,res)=>{
-    if(session.getItem('signin')==true && session.getItem("enrollstatus")==1){
+    if(session.getItem('signin')==true){
         var videotitle = session.getItem('videotitle');
         var playlistname = session.getItem('playlistname');
         var trainer = session.getItem('trainerid');
@@ -162,8 +188,6 @@ exports.getvideo=(req,res)=>{
         if(session.getItem("signin")==false){
             res.send("please signin first Thankyou");
         }
-        else if(session.getItem("enrollstatus")==0){
-            res.send("please enroll in the course first");
-        }
+        
     }
 }
